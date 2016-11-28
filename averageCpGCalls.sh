d8ee4366c7dc606dafc85650c328a006b4f032b2
Derek Tam <derek.tam.94@gmail.com>
2016-11-28 12:50:15 -0800

    Initial commit. Working!
    Script will search in directory for files matching RegEx, and take
    the average CpG methylation and output + or - depending on
    specified threshold value.
    
    * can take regex in -f to search for files within sample range
    * can take -c to set column of CpG calls
    * can take -p to set threshold for positive call
    
    TODO
    * still need to implement writing to file

---
+++ averageCpGCalls.sh
@@ -0,0 +1,38 @@
+
+#!/bin/bash
+
+if [ "$1" == "-h" ] ; then
+  echo -e "Average CpG calls for files matching RegEx
+  Usage: `basename $0` -i <dirIn> -o <dirOut> -f <files> -s <sample> -c <column> -p <pos>
+  <dirIn>: input directory
+  <dirOut>: output directory
+  <files>: files to be analyzed
+  <sample>: name of sample (for labelling)
+  <column>: column no. of CpG call in files
+  <pos>: threshold [0,1] for calling CIMP +"
+  exit 0
+fi
+
+## store input
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    -i) dirIn="$2"; shift;;
+    -o) dirOut="$2"; shift;;
+    -f) files="$2"; shift;;
+    -s) sample="$2"; shift;;
+    -c) col="$2"; shift;;
+    -p) pos="$2"; shift;;
+  esac
+  shift
+done
+
+## accumulate
+cd $dirIn
+for f in $files
+do
+
+  ## produce + for average above threshold; - otherwise
+  awk -v N=$col -v P=$pos '{ sum += $N } END { AVG = sum / NR; if (NR > 0 && AVG >= P) print "+"; else print "-" }' $f
+
+done
